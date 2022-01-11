03c5276c60cf7ba138be599c8affd416e2501e23
ridhisingla <ridsingla@gmail.com>
2022-01-10 23:34:28 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,19 +1,14 @@
 import streamlit as st
 import wget
-import requests
+
+
+st.title("My Streamlit App!")
+
+with st.form("app_form"):
+  status_code = st.radio("Pick a status code", ('101','102','405','406','407','416','417','500','502','521'))
   
-def main():
-    st.title("My Streamlit App!")
-    session = requests.Session()
-    with st.form("my_form"):
-        status_code = st.radio("Pick a status code", ('101','102','405','406','407','416','417','500','502','521'))
 
-        submitted = st.form_submit_button("Submit")
-
-        if submitted:
-            st.write("Result")
-            data = fetch(session, f"https://http.cat/{status_code}")
-            if data:
-                st.image(data['download_url'], caption=f"Author: {data['author']}")
-            else:
-                st.error("Error")
+  submitted = st.form_submit_button("Submit")
+  if submitted:
+    st.write("You pressed submit!")
+    st.image(wget.download("https://http.cat/" + status_code))
